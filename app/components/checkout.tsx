ba5da97ea34183b2fed3f73666a51009924a7572
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-06-12 15:04:09 +0200

    update status code

--- app/components/checkout.tsx
+++ app/components/checkout.tsx
@@ -199,7 +199,7 @@ const Checkout = () => {
       <div className="col-span-3">
         <div className="py-2 px-5 my-2 w-full bg-blue-100 text-blue-700 border border-blue-400 rounded">
           <strong>Current Status: </strong>
-          <code>{order?.status}</code>
+          <code>{order?.payment.status}</code>
         </div>
 
         {price && (
